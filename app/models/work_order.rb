c5e89690265730ef5d612c2afebf2d99a6dc207e
Jacob Woodruff <jkwoodruff93@gmail.com>
2023-04-04 16:08:53 -0400

    send notification when work order is assigned

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -1,3 +1,5 @@
+require 'push_notifications/notify'
+
 class WorkOrder < ApplicationRecord
     belongs_to :user, optional: true 
     # , foreign_key: "user_id"
@@ -6,5 +8,20 @@ class WorkOrder < ApplicationRecord
     validates :address, length: {minimum: 1, maximum: 100}, allow_blank: false
     validates :work_type, length: {minimum: 1, maximum: 30}, allow_blank: false
     validates :appointment_time, length: {minimum: 10, maximum: 30}, allow_blank: false
+
+    after_save :send_work_order_assigned_notification, if: :saved_change_to_user_id?, unless: :user_id_changed_to_zero?
+
+    def user_id_changed_to_zero?
+        self.user_id == 0
+    end
+   
+
+    def send_work_order_assigned_notification
+        if self.user_id != 0
+            puts "SENDING PUSH NOTIFICATION"
+            
+            Push::Notify.new_work_order(self)
+        end
+    end
 
 end
---
+++ lib/push_notifications/notify.rb
@@ -0,0 +1,42 @@
+
+module Push
+    class Notify
+      include HTTParty
+      HEADERS = { "Authorization" => "Bearer NzE2YzZlMzgtMGYxZC00MGMwLWFmOTAtOWI4NjEwYWMzN2E3", "Content-Type" => "application/json" }
+      # Create new file to storage log of pushes.
+      @push_logger = ::Logger.new(Rails.root.join('log', 'push.log'))
+      # Every request needs to inform the APP ID.
+      @body =  {
+        "app_id" => "e00d4c75-7ac6-4c3e-8589-9c124370b4fb"
+      }
+      def self.send_push(body, url)
+
+        result = HTTParty.post url, headers: HEADERS, body: body, logger: @push_logger, log_level: :debug, log_format: :curl
+        puts "RESULT:"
+        puts result
+      end
+      # Send push to all users.
+    #   def self.daily_news
+    #     push_body = @body.merge(
+    #       { 
+    #         "included_segments" => ["All"],
+    #         "url" => "https://onesignal.com",
+    #         "data" => { "type": "daily_news" },
+    #         "contents" => { "en" => "News!", "pt" =>  "Novidades!" }
+    #       }).to_json
+    #     send_push(push_body)
+    #   end
+      # Send push to specific user.
+      def self.new_work_order(work_order)
+        create_notification_url = "https://onesignal.com/api/v1/notifications"
+        push_body = @body.merge(
+          { 
+            "include_external_user_ids" => [work_order.user_id.to_s],
+            "contents" => { "en" => "You have received a new comment.", "pt" => "Você recebeu um novo comentário." }
+          }).to_json
+          puts "PUSH BODY:"
+          puts push_body
+        send_push(push_body, create_notification_url)
+      end
+    end
+  end
